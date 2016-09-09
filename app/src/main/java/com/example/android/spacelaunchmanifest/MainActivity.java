c30e9c9b79417936df37dc514bc3447fce2cb902
aaronoe <aaron.oe@live.de>
2016-09-08 23:10:21 -0700

    Implementing the custom ArrayAdapter with a random ArrayList of Launches

--- app/src/main/java/com/example/android/spacelaunchmanifest/MainActivity.java
+++ app/src/main/java/com/example/android/spacelaunchmanifest/MainActivity.java
@@ -6,31 +6,39 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LaunchArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<String> launchList = new ArrayList<>();
-        launchList.add("Falcon 9");
-        launchList.add("Atlas V");
-        launchList.add("Falcon Heavy");
-        launchList.add("CRS-5");
 
+
+        ArrayList<LaunchItem> launchList = new ArrayList<>();
+        launchList.add(new LaunchItem("SpaceX CRS-5", 1473401034, "https://google.de", "Falcon 9",
+                "CRS-5 Resupply Mission", "blablablarandomdescription"));
+        launchList.add(new LaunchItem("SpaceX Chicken", 1473121034, "https://google.de", "Delta V",
+                "CRS-5 Resupply Chicken Mission", "blablablarandomdescription"));
+        launchList.add(new LaunchItem("SpaceX Amos", 1473404334, "https://google.de", "Falcon Heavy",
+                "CRS-5 Resupply Mission", "blablablarandomdescription"));
+        launchList.add(new LaunchItem("SpaceX CRS-5", 1263401034, "https://google.de", "Falcon 9",
+                "CRS-5 Bacon Mission", "blablablarandomdescription"));
 
         // Find a reference to the {@link ListView} in the layout
-        ListView launchListView = (ListView) findViewById(R.id.list);
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
-        // Create a new {@link ArrayAdapter} of launchList
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, launchList);
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new LaunchArrayAdapter(this, launchList);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        launchListView.setAdapter(adapter);
+        earthquakeListView.setAdapter(mAdapter);
+
+
     }
 }
